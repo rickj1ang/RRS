351befbdf6eb412938786163909e494a0a2d72c4
rickj1ang <rick0j1ang@gmail.com>
2025-01-01 10:12:31 +0800

    new lord route

--- cmd/routes.go
+++ cmd/routes.go
@@ -10,6 +10,9 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /records/{id}", app.showRecordHandler)
 	mux.HandleFunc("DELETE /records/{id}", app.deleteRecordHandler)
 	mux.HandleFunc("PATCH /records/{id}", app.updateRecordHandler)
+	//user
+	mux.HandleFunc("POST /users", app.registerUserHandler)
+	mux.HandleFunc("GET /lord/{id}", app.givePowerHandler)
 	// this is a kind of custom Notfound page
 	// if the URL do not match the routes up
 	// stairs. app.notFoundResponse will out
