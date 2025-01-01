53ac39318641bbac4f62a2e3b03ca90ed4ce1d67
rickj1ang <rick0j1ang@gmail.com>
2025-01-01 10:13:26 +0800

    abstract some logic from main

--- cmd/server.go
+++ cmd/server.go
@@ -38,7 +38,17 @@ func (app *application) serve() error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		shutdownError <- srv.Shutdown(ctx)
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+
+		app.logger.PrintInfo("doing bakcground tasks", map[string]string{
+			"addr": srv.Addr,
+		})
+
+		app.wg.Wait()
+		shutdownError <- nil
 	}()
 
 	app.logger.PrintInfo("start servering", map[string]string{
