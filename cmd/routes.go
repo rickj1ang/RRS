68e51a5f5bdb81f6939c7b13aeee27c47b1ea487
rickj1ang <rick0j1ang@gmail.com>
2025-01-03 15:43:57 +0800

    new endpoint and middleware

--- cmd/routes.go
+++ cmd/routes.go
@@ -4,15 +4,15 @@ import "net/http"
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("GET /healthcheck", app.requireGod(app.healthcheckHandler))
 	mux.HandleFunc("GET /records", app.listRecordsHandler)
-	mux.HandleFunc("POST /records", app.createRecordHandler)
-	mux.HandleFunc("GET /records/{id}", app.showRecordHandler)
-	mux.HandleFunc("DELETE /records/{id}", app.deleteRecordHandler)
-	mux.HandleFunc("PATCH /records/{id}", app.updateRecordHandler)
+	mux.HandleFunc("POST /records", app.requireNormalUser(app.createRecordHandler))
+	mux.HandleFunc("GET /records/{id}", app.requireNormalUser(app.showRecordHandler))
+	mux.HandleFunc("DELETE /records/{id}", app.requireNormalUser(app.deleteRecordHandler))
+	mux.HandleFunc("PATCH /records/{id}", app.requireNormalUser(app.updateRecordHandler))
 	//user
 	mux.HandleFunc("POST /users", app.registerUserHandler)
-	mux.HandleFunc("GET /lord/{id}", app.givePowerHandler)
+	mux.HandleFunc("GET /lord/{id}", app.requireGod(app.givePowerHandler))
 
 	//authentication
 	mux.HandleFunc("POST /tokens/authentication", app.createAuthenticationTokenHandler)
@@ -21,5 +21,5 @@ func (app *application) routes() http.Handler {
 	// stairs. app.notFoundResponse will out
 	mux.HandleFunc("/", app.notFoundResponse)
 
-	return app.recoverPanic(app.rateLimit(mux))
+	return app.recoverPanic(app.rateLimit(app.authenticate(mux)))
 }
