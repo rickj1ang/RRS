9543945798379ee8f5fb73796fb9a6d1e25d5624
rickj1ang <rick0j1ang@gmail.com>
2025-01-14 10:55:12 +0800

    add a RabbitMQ model

--- internal/data/models.go
+++ internal/data/models.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/mongo"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 var ErrRecordNotFound = errors.New("record not found")
@@ -13,12 +15,14 @@ type Models struct {
 	Records RecordModel
 	Users   UserModel
 	Tokens  TokenRedis
+	Notify  NotifyQue
 }
 
-func NewModels(cl *mongo.Client, rd *redis.Client) Models {
+func NewModels(cl *mongo.Client, rd *redis.Client, conn *amqp.Connection) Models {
 	return Models{
 		Records: RecordModel{CL: cl},
 		Users:   UserModel{CL: cl},
 		Tokens:  TokenRedis{CL: rd},
+		Notify:  NotifyQue{Conn: conn},
 	}
 }
